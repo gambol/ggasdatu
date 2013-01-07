2721580db05287ffbc04a4266eeda7caa7297336
happyzzb@gmail.com <happyzzb@gmail.com@2a70d515-9449-7094-ba0d-9bf944cc24df>
2013-01-07 09:02:25 +0000

    get some codes format

--- trunk/src/com/tophey/web/controller/PublishController.java
+++ trunk/src/com/tophey/web/controller/PublishController.java
@@ -29,152 +29,151 @@ import com.tophey.utils.DateUtil;
 
 /**
  * 用户主动发布站点
- *
+ * 
  * @author zhenbao.zhou
  */
+@SuppressWarnings("unchecked")
 @Controller
 @RequestMapping("/user/publish")
 @SessionAttributes("publisBean")
 public class PublishController {
-    final private Logger logger = Logger.getLogger(this.getClass().getName());
+	private final Logger logger = Logger.getLogger(this.getClass().getName());
 
-    public static HashMap NETWORK_MAP = new HashMap();
-    
-    static {
-        NETWORK_MAP.put(0, "网通");
-        NETWORK_MAP.put(1, "电信");
-        NETWORK_MAP.put(2, "双线");
-        NETWORK_MAP.put(3, "联通");
-        NETWORK_MAP.put(4, "铁通");
-        
-        NETWORK_MAP.put("网通", 0);
-        NETWORK_MAP.put("电信", 1);
-        NETWORK_MAP.put("双线", 2);
-        NETWORK_MAP.put("联通", 3);
-        NETWORK_MAP.put("铁通", 4);
-    }
-    // Invoked initially to create the "form" attribute
-    // Once created the "form" attribute comes from the HTTP session (see @SessionAttributes)
+	@SuppressWarnings("rawtypes")
+	public static HashMap NETWORK_MAP = new HashMap();
 
-    @ModelAttribute("publishBean")
-    public PublishBean createFormBean() {
-        return new PublishBean();
-    }
+	static {
+		NETWORK_MAP.put(0, "网通");
+		NETWORK_MAP.put(1, "电信");
+		NETWORK_MAP.put(2, "双线");
+		NETWORK_MAP.put(3, "联通");
+		NETWORK_MAP.put(4, "铁通");
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String form(HttpServletRequest request, HttpServletResponse response, Model model) throws ServletException, IOException {
-        int userId = 0;
-        int serverId = 0;
-        String username = (String)request.getSession().getAttribute(SessionConst.USERNAME);
-        System.out.println("username:" + username);
-        PublishBean pb = new PublishBean();
-     
-        try {
-            userId = (Integer)request.getSession().getAttribute(SessionConst.USERID);
-        } catch(Exception e) {
-            e.printStackTrace();
-            return "redirect:/user/login.htm";
-        }
-        
-        try {
-            
-            String strId = request.getParameter("id");
-            if (strId != null)
-                serverId = Integer.parseInt(request.getParameter("id"));
-        } catch(Exception e) {
-            e.printStackTrace();
-            pb.setNetwork("双线");
-            model.addAttribute("publishBean", pb);
-            return "publish";
-        }
-        
-        // 查询db
-        ServerInfo si = ServerDao.getServerInfoById(serverId);
-        if (si == null) {
-            pb.setNetwork("双线");
-            model.addAttribute("publishBean", pb);
-            return "publish";
-        }
-        
-        pb.setServer_name(si.getName());
-        pb.setBanner(si.getBannerUrl());
-        pb.setCategory(si.getCategoryId());
-        pb.setNetwork(si.getLine());
+		NETWORK_MAP.put("网通", 0);
+		NETWORK_MAP.put("电信", 1);
+		NETWORK_MAP.put("双线", 2);
+		NETWORK_MAP.put("联通", 3);
+		NETWORK_MAP.put("铁通", 4);
+	}
 
-        pb.setDesc(si.getDescription());
-        pb.setUrl(si.getUrl());
-        pb.setId(si.getId());
-        
-        model.addAttribute("publishBean", pb);
-        return "publish";
-    }
+	// Invoked initially to create the "form" attribute
+	// Once created the "form" attribute comes from the HTTP session (see
+	// @SessionAttributes)
 
-    @RequestMapping(method = RequestMethod.POST)
-    public String processSubmit(@Valid PublishBean publishBean, BindingResult result,
-    HttpServletRequest request, HttpServletResponse response, 
-            Model model) {
+	@ModelAttribute("publishBean")
+	public PublishBean createFormBean() {
+		return new PublishBean();
+	}
 
-        if (result.hasErrors()) {
-            System.err.println(result.toString() + "   " + (result.getAllErrors()).get(0).toString());
-            return null;
-        }
-        
-        int userId = 0;
-        try {
-            userId = (Integer)request.getSession().getAttribute(SessionConst.USERID);
-        } catch(Exception e) {
-            e.printStackTrace();
-            return "redirect:/user/login.htm";
-        }
-        
-        if (userId == 0) {
-            logger.log(Level.WARNING, "user not logined");
-            return "redirect:/user/login.htm";
-        }
+	@RequestMapping(method = RequestMethod.GET)
+	public String form(HttpServletRequest request, HttpServletResponse response, Model model) throws ServletException,
+			IOException {
+		int userId = 0;
+		int serverId = 0;
+		String username = (String) request.getSession().getAttribute(SessionConst.USERNAME);
+		System.out.println("username:" + username);
+		PublishBean pb = new PublishBean();
 
-        ServerInfo server = new ServerInfo();
- //       server.setTitle(publishBean.getServer_name());
-        server.setBannerUrl(publishBean.getBanner());
-        server.setCategoryId(publishBean.getCategory());
+		try {
+			userId = (Integer) request.getSession().getAttribute(SessionConst.USERID);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+			return "redirect:/user/login.htm";
+		}
 
-        server.setLine(publishBean.getNetwork());
-        server.setName(publishBean.getServer_name());
-        server.setUrl(publishBean.getUrl());
-        
-        server.setSiteFrom("atu");
-        server.setCreateDate(DateUtil.getCurrentTimestamp());
-        server.setDescription(publishBean.getDesc());
-        server.setUserId(userId);
-        
-        int sid  = -1;
-        try {
-            sid = publishBean.getId();
-        } catch(Exception ex) {
-            
-        }
-        
-        if (sid <= 0) {
-            ServerDao.insert(server);            
-        } else {
- //           server.setId(sid);
-            if (server.getUserId() != userId) {
-                logger.log(Level.WARNING, "server对应的userId 和 新的UserId对应不上. old userId:" + server.getUserId() + " new UserId:" + userId);
-                model.addAttribute("message", "session过期");
-                return null;
-            }
-            
-            ServerDao.update(server);
-        }
-        
-        
-        
-        String message = "私服发布成功, 私服url:" + server.getUrl();
-        // Success response handling
+		try {
+			String strId = request.getParameter("id");
+			if (strId != null) {
+				serverId = Integer.parseInt(request.getParameter("id"));
+			}
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+			pb.setNetwork("双线");
+			model.addAttribute("publishBean", pb);
+			return "publish";
+		}
 
-        // prepare model for rendering success message in this request
-  //      model.addAttribute("message", message);
-        // 确认这个页面之后，需要跳转到哪个页面?
-        return "redirect:/user/sitemanage.htm";
+		// 查询db
+		ServerInfo si = ServerDao.getServerInfoById(serverId);
+		if (si == null) {
+			pb.setNetwork("双线");
+			model.addAttribute("publishBean", pb);
+			return "publish";
+		}
 
-    }
+		pb.setServerName(si.getName());
+		pb.setBanner(si.getBannerUrl());
+		pb.setCategory(si.getCategoryId());
+		pb.setNetwork(si.getLine());
+
+		pb.setDesc(si.getDescription());
+		pb.setUrl(si.getUrl());
+		pb.setId(si.getId());
+
+		model.addAttribute("publishBean", pb);
+		return "publish";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String processSubmit(@Valid PublishBean publishBean, BindingResult result, HttpServletRequest request,
+			HttpServletResponse response, Model model) {
+
+		if (result.hasErrors()) {
+			System.err.println(result.toString() + "   " + (result.getAllErrors()).get(0).toString());
+			return null;
+		}
+
+		int userId = 0;
+		try {
+			userId = (Integer) request.getSession().getAttribute(SessionConst.USERID);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "redirect:/user/login.htm";
+		}
+
+		if (userId == 0) {
+			logger.log(Level.WARNING, "user not logined");
+			return "redirect:/user/login.htm";
+		}
+
+		ServerInfo server = new ServerInfo();
+		// server.setTitle(publishBean.getServer_name());
+		server.setBannerUrl(publishBean.getBanner());
+		server.setCategoryId(publishBean.getCategory());
+
+		server.setLine(publishBean.getNetwork());
+		server.setName(publishBean.getServerName());
+		server.setUrl(publishBean.getUrl());
+
+		server.setSiteFrom("atu");
+		server.setCreateDate(DateUtil.getCurrentTimestamp());
+		server.setDescription(publishBean.getDesc());
+		server.setUserId(userId);
+
+		int sid = -1;
+		sid = publishBean.getId();
+
+		if (sid <= 0) {
+			ServerDao.insert(server);
+		} else {
+			// server.setId(sid);
+			if (server.getUserId() != userId) {
+				logger.log(Level.WARNING, "server对应的userId 和 新的UserId对应不上. old userId:" + server.getUserId()
+						+ " new UserId:" + userId);
+				model.addAttribute("message", "session过期");
+				return null;
+			}
+
+			ServerDao.update(server);
+		}
+
+		String message = "私服发布成功, 私服url:" + server.getUrl();
+		// Success response handling
+
+		// prepare model for rendering success message in this request
+		// model.addAttribute("message", message);
+		// 确认这个页面之后，需要跳转到哪个页面?
+		return "redirect:/user/sitemanage.htm";
+
+	}
 }
--- trunk/src/com/tophey/web/dao/CategoryDao.java
+++ trunk/src/com/tophey/web/dao/CategoryDao.java
@@ -11,22 +11,21 @@ import java.util.LinkedHashMap;
 import com.tophey.utils.JDBCUtils;
 
 /**
- *
+ * 
  * @author zhenbao.zhou
  */
 public class CategoryDao {
-    
-    /**
-     * 列举所有的私服分类
-     * display_order 越大,排在月前面
-     * 按照display 逆序, id 顺序
-     * @return 
-     */
-      public static PageResult<Category> getAllCategory() {
-       LinkedHashMap<String, Boolean> orderBy = new LinkedHashMap<String, Boolean>();
-       orderBy.put("display_order", true);
-       orderBy.put("id", false);
-       PageResult<Category> pr = JDBCUtils.getPageData(Category.class, 200, 1, orderBy, " is_disabled = 0");
-       return pr;   
-    }
+
+	/**
+	 * 列举所有的私服分类 display_order 越大,排在月前面 按照display 逆序, id 顺序
+	 * 
+	 * @return
+	 */
+	public static PageResult<Category> getAllCategory() {
+		LinkedHashMap<String, Boolean> orderBy = new LinkedHashMap<String, Boolean>();
+		orderBy.put("display_order", true);
+		orderBy.put("id", false);
+		PageResult<Category> pr = JDBCUtils.getPageData(Category.class, 200, 1, orderBy, " is_disabled = 0");
+		return pr;
+	}
 }
